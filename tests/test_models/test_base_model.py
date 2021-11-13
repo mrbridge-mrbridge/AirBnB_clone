6273ec69010b3fc43f563f1bbba0c05b8ebf9079
layan2k <lesliedeeshumba@gmail.com>
2021-11-13 20:29:19 +0200

    test file init

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Unit testing for the base model"""
+
+import unittest
+import pep8
+import cmd
+from models.base_model import Basemodel
+import models
+from datetime import datetime
+import sys
+from os import
+
