6b3e742b1f9278d417e1c208ddd0cff8a90f6f49
layan2k <lesliedeeshumba@gmail.com>
2021-11-13 21:15:27 +0200

    test files init

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,8 +5,34 @@ import unittest
 import pep8
 import cmd
 from models.base_model import Basemodel
-import models
-from datetime import datetime
-import sys
-from os import
 
+
+class test_basemodel(unittest.TestCase):
+    """Test Model for the Base Model"""
+    def test_pep8(self):
+        """Tests if the code is pep8 complaint"""
+        pycodestlye = pep8.StyleGuide(quiet=True)
+        result = pycodestlye.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Fails Pep8 complaince")
+
+    def test_file(self):
+        """Tests if file exists"""
+        self.assertTrue(isflie('models/base_model.py'),'File missing' )
+
+class test_dict(unittest.TestCase):
+    """Dict Basemodel"""
+
+    def startUp(self):
+        '''Set Up'''
+        self.dict1 = BaseModel().to_dict()
+        self.dict2 = BaseModel().to_dict()
+
+    def test_dict(self):
+        self.assertTrue(self.dict1, dict,
+        "Failed to generate dictionary" "type")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
