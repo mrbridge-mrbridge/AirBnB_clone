e9c62c4f2388c489b895d466ed1469e7a16d5a00
layan2k <lesliedeeshumba@gmail.com>
2021-11-14 00:21:56 +0200

    import and string lit change

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,7 +5,7 @@ Test Amenity
 
 from datetime import datetime
 import inspect
-from models import Amenity
+from models.Amenity import Amenity
 from models.base_model import BaseModel
 import pep8
 import unittest
@@ -98,7 +98,7 @@ class TestAmenity(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in am.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
